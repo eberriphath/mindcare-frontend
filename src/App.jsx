63066d1a2529c00e6de8712f13b8882a1d9ce2fb
eberriphath <riphatheber@gmail.com>
2025-10-21 12:27:15 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login"; // ✅ lowercase because your file is 'login.jsx'
+import Login from "./pages/login"; 
 
 function App() {
   return (
